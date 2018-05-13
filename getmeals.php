77722d60450cdca3dce3e2bb8a0d15e5aa6933df
Patel Jinesh <patelj60@mcmaster.ca>
2018-05-13 12:27:44 -0400

    added code to fetch rows from db and return json of meals

--- getmeals.php
+++ getmeals.php
@@ -1,11 +1,16 @@
 <?php
     $servername = "localhost";
-    $username = "";
-    $password = "";
-    $database = "";
+    $username = "id5743664_macmeals";
+    $password = "seitb";
+    $database = "id5743664_meals";
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+        $query = $conn->prepare("SELECT * FROM meals");
+        $query = $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result);
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
