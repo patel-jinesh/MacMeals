a2a1eae30af1ac311c10f10de9028a2b251a4e1e
Patel Jinesh <patelj60@mcmaster.ca>
2018-05-14 12:51:30 -0400

    added script to add a meal to database

---
+++ addmeal.php
@@ -0,0 +1,26 @@
+<?php
+    $servername = "localhost";
+    $username = "id5743664_macmeals";
+    $password = "seitb";
+    $database = "id5743664_meals";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+        $meal = $_POST["meal"];
+        $location = $_POST["location"];
+        $rating = $_POST["rating"];
+        $review = $_POST["review"];
+
+        $query = $conn->prepare("INSERT INTO meals (meal, location, rating, review) VALUES (?, ?, ?, ?)");
+        $query->bind_param("ssis", $meal, $location, $rating, $review);
+        $query->execute();
+
+        $query->close();
+        $conn->close();
+
+        echo "Success";
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
